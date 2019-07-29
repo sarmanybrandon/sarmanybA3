6b578204df96fff7550f985f685123b3df012ee1
sarmanybrandon <52832686+sarmanybrandon@users.noreply.github.com>
2019-07-28 20:12:47 -0400

    Add files via upload

---
+++ github/assets/Data/StudentInfo.ts
@@ -0,0 +1,9 @@
+import { Student } from '../../app/sarmanyb';
+export const STUDENTINFO: Student = 
+{
+    sid: 991403874,
+    sname: 'Brandon Sarmany',
+    slogin: 'sarmanyb',
+    scampus: 'Trafalgar',
+    stitle: 'Assignment Three'
+}
