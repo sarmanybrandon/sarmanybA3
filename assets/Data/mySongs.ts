9f1b4d025e1962563a5199896987075cd4eb103f
sarmanybrandon <52832686+sarmanybrandon@users.noreply.github.com>
2019-07-28 20:20:21 -0400

    Add files via upload

---
+++ assets/Data/mySongs.ts
@@ -0,0 +1,33 @@
+import {songInfo} from '../../app/songInfo';
+
+export const mySongs: songInfo[] =[
+    {
+    name: "King Konta",
+    artist: "Kendrick Lamar",
+    genre: "Rap",
+    releaseYear: "2015",
+    picture: "./assets/pictures/kingKunta.jpeg"
+    },
+    {
+    name: "Fancy Clown",
+    artist: "Madvillain, Viktor Vaughn",
+    genre: "Rap",
+    releaseYear: "2004",
+    picture: "./assets/pictures/madVill.png"
+    },
+    {
+    name: "Lucky You",
+    artist: "Eminem, Joyner Lucas",
+    genre: "Rap",
+    releaseYear: "2018",
+    picture: "./assets/pictures/kamikaze.jpg"
+    },
+    {
+    name: "Like Toy Soldiers",
+    artist: "Eminem",
+    genre: "Rap",
+    releaseYear: "2004",
+    picture: "./assets/pictures/toySoldier.jpg"
+    }
+
+]
